6ddf61eebc1c3166c96ca8f1ab36b69193fe7639
F.Salustri <fs314@kent.ac.uk>
2019-08-07 14:17:21 +0100

    added simple menu with options to parse files, translate files and show which conditions are found in file

--- KsParser.java
+++ KsParser.java
@@ -16,6 +16,7 @@ public class KsParser
 	ReadKSFile rks;
 	SplitCondition sp;
 	WriteParsedKS wp;
+
 	
 	/**
 	* Constructor
@@ -26,6 +27,7 @@ public class KsParser
 		sp = new SplitCondition();
 		wp = new WriteParsedKS();
 		
+	
 	} 
 	
 	/**
@@ -35,8 +37,21 @@ public class KsParser
 	public static void main (String[] args)  throws FileNotFoundException
 	{
 		KsParser ksp = new KsParser();
-	
-		ksp.getParsedFiles(args[0], args[1]);
+	    
+		if(args[0].equals("-tr")){
+			ksp.printContent(args[1]);
+		} else if (args[0].equals("-pr")){
+		    ksp.getParsedFiles(args[1], args[2]);
+		} else if (args[0].equals("-sc")){
+			ksp.showConditions(args[1]);
+		} else if(args[0].equals("-hp")){
+			System.out.println(" ");
+			System.out.println("TRANSLATE JSON: -tr NameOfFileToTranslate");
+			System.out.println("SHOW CONDITIONS: -sc NameOfFileToTranslate");
+			System.out.println("PARSE JSON: -pr FromFolder  ToFolder");
+			System.out.println("HELP: -hp to know how the parser works");
+			System.out.println(" ");
+		}
 	}
 	
 	/**
@@ -53,14 +68,31 @@ public class KsParser
 		
 	} 
 	
-	/**
+	 /**
 	* Main method. 
 	* @param 
 	**/
-	/*
-	public void stringify() throws FileNotFoundException
+	public void showConditions(String filename) throws FileNotFoundException
 	{
-		
-	} */
+		for (int i=0; i<sp.getFlags().size(); i++) 
+		{
+			Gson gson = new Gson();
+			System.out.println(" ");
+			System.out.println("CONDITION " + sp.getFlags().get(i));
+			System.out.println(sp.flagDelimiter(sp.fromAscii(rks.getLetterCodes(rks.getKsData (rks.getFiles(filename), gson))), sp.getFlags().get(i)));
+		    System.out.println(" ");
+		}
+	} 
+	
+   /**
+	* Main method. 
+	* @param 
+	**/
+	public void printContent(String filename) throws FileNotFoundException
+	{
+		Gson gson = new Gson();
+		System.out.println(" ");
+		System.out.println(sp.fromAscii(rks.getLetterCodes(rks.getKsData (rks.getFiles(filename), gson)))); 
+	} 
 	
 }
